8ee11befb5e15447c526dc501bd3a398a08ae5e4
Moon Jongmin <whdals0@naver.com>
2020-04-16 15:31:28 +0900

    1. SignUp XML 페이지 디자인

--- app/src/main/java/com/roopre/firstapp/MainActivity.java
+++ app/src/main/java/com/roopre/firstapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.roopre.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    // 버튼 이나 뷰 선언 부분
     Button signin_btn;
     Button signup_btn;
     ImageView main_iv;
@@ -16,23 +19,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signin);
 
+        // 선언한 뷰 초기화
         signin_btn = findViewById(R.id.signin_btn);
         signup_btn = findViewById(R.id.signup_btn);
         main_iv = findViewById(R.id.main_iv);
 
+        // 초기화한 뷰에 대한 이벤트 리스터 추가
         signin_btn.setOnClickListener(this);
         signup_btn.setOnClickListener(this);
         main_iv.setOnClickListener(this);
 
     }
 
+    // 실질적인 이벤트 구현부
     @Override
     public void onClick(View view) {
         switch(view.getId()){
             case R.id.signup_btn:
                 //Toast.makeText(this, "Signup Button Clicked!!!!!!", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(this, SignupActivity.class);
+                startActivityForResult(intent, 100);
+
+
                 break;
             case R.id.signin_btn:
                 Toast.makeText(this, "SignIn Clicked!!!!!", Toast.LENGTH_SHORT).show();
